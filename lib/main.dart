4ecfb8d5945c47f4d492f621ae530f66f10d9051
Lucas <lucasbarbosa1738@gmail.com>
2020-11-07 10:43:07 -0300

    commit inicial

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class Formulario extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Cadastrando produto'),
+          title: Text('Formulário de Produtos'),
         ),
         body: Padding(
           padding: const EdgeInsets.all(5.0),
