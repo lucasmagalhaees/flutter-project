0e993c82241f0998818b3a2d2e9a598d3657baa4
Lucas <lucasbarbosa1738@gmail.com>
2020-11-28 18:18:29 -0300

    projeto final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'br/com/projeto/flutter/User.dart';
 
 void main() {
   runApp(MyApp());
@@ -47,74 +51,124 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  bool visibility = true;
+
+
+
+class Item extends StatelessWidget {
+  final User usuario;
+  final bool changeItem;
+
+  Item(this.usuario, this.changeItem);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(backgroundColor: new Color(0xFF26C6DA)),
-      body: new Container(
-        margin: new EdgeInsets.only(left: 16.0, right: 16.0),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              if (visibility)
-                Expanded(
-                  child: FittedBox(
-                    fit: BoxFit.contain, // otherwise the logo will be tiny
-                    child: const FlutterLogo(),
-                  ),
-                )
-            ]),
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.streetview),
+        title: Text(this.usuario.id.toString() + " - " + this.usuario.name.toString() ),
+        subtitle: Text(changeItem ? "Telefone: " + this.usuario.phone.toString() : "Email: " + this.usuario.email.toString()),
       ),
-      floatingActionButton: Row(
+    );
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List<User> _usuarios;
+  int _size;
+  bool isPhone = false;
+
+  Future<List<User>> _getCategorias() async{
+
+    try{
+      List<User> listUsuarios = List();
+      final response = await http.get('https://jsonplaceholder.typicode.com/users');
+      if(response.statusCode == 200){
+        var decodeJson = jsonDecode(response.body);
+        decodeJson.forEach((item) => 
+         listUsuarios.add(User.fromJson(item)));
+      return listUsuarios;
+      } else {
+        print('Erro ao carregar lista');
+        return null;
+      }
+
+    } catch(e){
+
+        print('Erro ao carregar lista');
+        return null;
+    }
+  }
+  
+
+  @override
+  void initState(){
+    super.initState();
+    _getCategorias().then((map){
+    _usuarios = map;
+      _usuarios.removeWhere((element) => element.id > 5);
+      _size = _usuarios.length;
+      print(_usuarios.length);
+    });
+  }
+  
+
+     @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: _size,
+          itemBuilder: (BuildContext context, int index){
+            return Item(_usuarios[index], isPhone);
+          }),
+      ),
+        appBar: new AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Listagem de Usuários - Top 5'),
+      ),
+
+        floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
           FloatingActionButton(
+            backgroundColor: Colors.red,
             onPressed: () {
-              _show();
+              _exibeTelefone();
             },
             tooltip: 'Incrementar',
-            child: Icon(Icons.remove_red_eye),
+            child: Icon(Icons.phone),
           ),
           SizedBox(
             width: 10.0,
           ),
           FloatingActionButton(
+            backgroundColor: Colors.orange,
             onPressed: () {
-              _hide();
+              _exibeEmail();
             },
             tooltip: 'Decrementar',
-            child: Icon(Icons.close),
+            child: Icon(Icons.email),
           ),
         ],
-      ),
+    ),
+     
     );
   }
-
-  void _show() {
+   void _exibeTelefone() {
     setState(() {
-      visibility = true;
+      isPhone = true;
     });
   }
 
-  void _hide() {
+  void _exibeEmail() {
     setState(() {
-      visibility = false;
+      isPhone = false;
     });
   }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  
 
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
+
+
 }
